2043cbbaae42dda1ee186ee49f43e269583ee34b
Calin <campean.calin@gmail.com>
2022-12-17 22:47:23 +0200

    clasa Car

---
+++ CarRunner.java
@@ -0,0 +1,10 @@
+package backToTech
+public class CarRunner {
+
+    public static void main(String[] args) {
+            Car car = new Car();
+            car.setModel("VW Caddy);
+            System.out.println("Model is " + car.getModel());
+        }
+}
+
